fa4ab2eb881545f3bb4828a466f4805689c56772
KhoiLuu143 <khoiluub143@gmail.com>
2025-06-06 10:01:52 +0700

    Initial setup

---
+++ src/main/java/com/protrack/protrack_be/ProtrackBeApplication.java
@@ -0,0 +1,13 @@
+package com.protrack.protrack_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtrackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtrackBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protrack/protrack_be/ProtrackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protrack.protrack_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtrackBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
