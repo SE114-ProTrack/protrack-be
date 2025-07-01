7260c8324373273c13de822148770cbafba6cd33
DiMinch <phanduyminh1262@gmail.com>
2025-07-01 16:50:16 +0700

    feat: new routes to get user for a new conversation

--- src/main/java/com/protrack/protrack_be/repository/UserRepository.java
+++ src/main/java/com/protrack/protrack_be/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +16,22 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query("SELECT u FROM User u WHERE u.account.email = :email")
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query(value = """
+    SELECT DISTINCT u.*
+    FROM thanhvienduan pm1
+    JOIN thanhvienduan pm2 ON pm1.id_duan = pm2.id_duan
+    JOIN nguoidung u ON u.id_nguoidung = pm2.id_nguoidung
+    WHERE pm1.id_nguoidung = :currentUserId
+      AND pm2.id_nguoidung <> :currentUserId
+      AND NOT EXISTS (
+          SELECT 1 FROM tinnhan t
+          WHERE 
+              (t.id_nguoigui = :currentUserId AND t.id_nguoinhan = pm2.id_nguoidung)
+              OR
+              (t.id_nguoinhan = :currentUserId AND t.id_nguoigui = pm2.id_nguoidung)
+      )
+    """, nativeQuery = true)
+    List<User> findUsersInSameProjectWithoutConversation(@Param("currentUserId") UUID currentUserId);
+
 }
